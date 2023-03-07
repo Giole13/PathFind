09ec93079383e7cd70d44cab4075d96dbd272512
Giole13 <gudwns4f12@gmail.com>
2023-03-07 16:36:06 +0900

    [HJ] Back-Up

---
+++ PathFind/Assets/01.UnityProject/Scripts/PlayScene/LeftUiButtons.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftUiButtons : MonoBehaviour
+{
+    //! A star find path 버튼을 누른경우
+    public void OnClickAstarFindBtn()
+    {
+        GioleFunc.Log("[LeftUiButtons] OnClickAstarFindBtn : 길찾기 버튼 클릭!");
+    }       // OnClickAstarFindBtn()
+
+}
